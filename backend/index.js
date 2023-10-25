4dfb039cd535dddec1f7c152c02ca79c8fe748f1
Advaita <vedanta3000@gmail.com>
2023-10-25 19:28:56 +0530

    Created basic home page

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {PORT , mongoDBURL} from "./config.js";
+import {PORT} from "./config.js";
 import mongoose from "mongoose";
 //import {Book} from "./models/bookmodel.js";
 import bookRoute from "./routers/bookRoute.js";
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "./paths/Home.js";
+import CreateBook from "./paths/CreateBook.js";
+import DeleteBook from "./paths/DeleteBook.js";
+import EditBook from "./paths/EditBook.js";
+import ShowBook from "./paths/ShowBook.js";
 
 
 function App() {
   return (
-    <>
-      <div className="text-3xl font-bold underline">Hello World</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/books/create" element={<CreateBook/>} />
+      <Route path="/books/details/:id" element={<ShowBook/>} />
+      <Route path="/books/edit/:id" element={<EditBook/>} />
+      <Route path="/books/delete/:id" element={<DeleteBook/>} />
+    </Routes>
   );
 }
 
